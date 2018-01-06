52952aab18024af49a8bc2e8dc7a82cf44badd13
hdudowicz <hdudowicz@gmail.com>
2018-01-06 19:55:33 +0000

    GUI Implementation

--- b_implementation/bank_system.py
+++ b_implementation/bank_system.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 import csv
 import os
 import random
@@ -8,19 +8,37 @@ from account import Account
 from admin import Admin
 from customer import Customer
 from tkinter import *
+from tkinter import ttk
 from datetime import datetime, timedelta
 
 customers_list = []
 admins_list = []
 
 
-class BankSystem(object):
+class BankSystem(tk.Tk):
     def __init__(self):
-        self.customers_list = []
-        self.admins_list = []
         self.load_bank_data()
-        self.mw = tkinter.Tk()
 
+        tk.Tk.__init__(self)
+
+        container = Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (MainMenu, CustomerMenu, AdminMenu):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(MainMenu)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
     def load_bank_data(self):
         account_no = 1234
@@ -35,113 +53,67 @@ class BankSystem(object):
                     currentCustomer = customers[currentIndex]
                     currentAcc = Account(row[7], account_no)
                     accounts.append(currentCustomer.open_account(currentAcc))
-                    self.customers_list.append(currentCustomer)
+                    customers_list.append(currentCustomer)
                     currentIndex = currentIndex + 1
                 elif row[0] == "Admin":
-                    #Combine accouts and customer/admin or open accounts with for loop after readcsv
+                    # Combine accounts and customer/admin or open accounts with for loop after readcsv
                     customers.append(Admin(row[1], row[2], True, [row[3], row[4], row[5], row[6]]))
                     currentAdmin = customers[currentIndex]
                     currentAcc = Account(row[7], account_no)
-                    self.admins_list.append(currentAdmin)
+                    admins_list.append(currentAdmin)
                     currentIndex = currentIndex + 1
-                #Applying fee if its past loan return date
+                # Applying fee if its past loan return date
                 if currentAcc.getLoanAmount() != 0 and currentAcc.getReturnDate() > datetime.now():
                     balance = currentAcc.get_balance()
                     currentAcc.set_balance(balance - 50)
 
-    def customer_login(self, name, password):
-        #STEP A.1
-        found_customer = self.search_customers_by_name(name)
-        if found_customer == None:
-            return "The customer has not been found!\n"
-        else:
-            if found_customer.check_password(password) == True:
-                self.run_customer_options(found_customer)
-            else:
-                return "you have input a wrong password"
-        
-    def search_customers_by_name(self, customer_name):
-        #STEP A.2
-        found_customer = None
-        for a in self.customers_list:
-            name = a.get_name()
-            if name == customer_name:
-                found_customer = a
-                break
-        if found_customer == None:
-            print("\nThe customer %s does not exist! Try again...\n" % customer_name)
-        return found_customer
-
-    def admin_login(self, name, password):
-        # STEP A.3
-        found_admin = self.search_admin_by_name(name)
-        if found_admin == None:
-            return "\n The admin has not been found!\n"
-        else:
-            if (found_admin.check_password(password) == True):
-                self.run_admin_options(found_admin)
-            else:
-                return "you have input a wrong password"
-
-    def search_admin_by_name(self, admin_name):
-        # STEP A.4
-        found_admin = None
-        for a in self.admins_list:
-            name = a.get_name()
-            if name == admin_name:
-                found_admin = a
-                break
-        if found_admin == None:
-            print("\nThe admin %s does not exist! Try again...\n" % admin_name)
-        return found_admin
-
     def main_menu(self):
-        #print the options you have
+        app.mainloop()
+        # print the options you have
         print()
         print()
-        print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print ("Welcome to the Python Bank System")
-        print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print ("1) Admin login")
-        print ("2) Customer login")
-        print ("3) Quit Python Bank System")
-        print (" ")
-        option = int(input ("Choose your option: "))
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Welcome to the Python Bank System")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("1) Admin login")
+        print("2) Customer login")
+        print("3) Quit Python Bank System")
+        print(" ")
+        option = int(input("Choose your option: "))
         return option
 
     def run_main_option(self):
-        loop = 1         
+        loop = 1
         while loop == 1:
             choice = self.main_menu()
             if choice == 1:
-                name = input ("\nPlease input admin name: ")
-                password = input ("\nPlease input admin password: ")
+                name = input("\nPlease input admin name: ")
+                password = input("\nPlease input admin password: ")
                 msg = self.admin_login(name, password)
                 print(msg)
             elif choice == 2:
-                name = input ("\nPlease input customer name: ")
-                password = input ("\nPlease input customer password: ")
+                name = input("\nPlease input customer name: ")
+                password = input("\nPlease input customer password: ")
                 msg = self.customer_login(name, password)
                 print(msg)
             elif choice == 3:
                 loop = 0
-        print ("Thank-You for stopping by the bank!")
-        
+        print("Thank-You for stopping by the bank!")
 
     def customer_menu(self, customer_name):
-        #print the options you have
-         print (" ")
-         print ("Welcome %s : Your transaction options are:" %customer_name)
-         print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-         print ("1) Transfer money")
-         print ("2) Other account operations")
-         print ("3) profile settings")
-         print("4) Request Loan")
-         print("5) Return Loan")
-         print ("6) Sign out")
-         print (" ")
-         option = int(input ("Choose your option: "))
-         return option
+        # print the options you have
+        print(" ")
+        print("Welcome %s : Your transaction options are:" % customer_name)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("1) Transfer money")
+        print("2) Other account operations")
+        print("3) profile settings")
+        print("4) Request Loan")
+        print("5) Return Loan")
+        print("6) Sign out")
+        print(" ")
+        option = int(input("Choose your option: "))
+        return option
 
     def transferMoney(self, fromAcc, toAcc, amount):
         tempFile = "tmp.csv"
@@ -165,10 +137,9 @@ class BankSystem(object):
                 else:
                     writeCSV.writerow(row)
         os.replace(tempFile, "accounts.csv")
-    
+
     def run_customer_options(self, customer):
-                    
-        account = customer.get_account()            
+        account = customer.get_account()
         loop = 1
         while loop == 1:
             choice = self.customer_menu(customer.get_name())
@@ -180,21 +151,21 @@ class BankSystem(object):
                 account.run_account_options()
             elif choice == 3:
                 customer.run_profile_options()
-            elif choice ==4:
+            elif choice == 4:
                 requestAmount = int(input("How much would you like to loan: "))
                 rand = random.randint(1, 11)
                 balance = customer.get_account().get_balance()
-                #implement backend storage and add     and rand <  3
-                if requestAmount <= 10000 :
+                # implement backend storage and add     and rand <  3
+                if requestAmount <= 10000:
                     print("Loan Accepted")
-                    customer.get_account().set_balance(balance+requestAmount)
+                    customer.get_account().set_balance(balance + requestAmount)
                     returnDate = datetime.now() + timedelta(days=21)
                     customer.get_account().setReturnDate(returnDate)
                     customer.get_account().setLoanAmount(requestAmount)
                     print("Loan must be returned by " + str(returnDate))
                 else:
                     print("Loan Not Accepted")
-                    
+
             elif choice == 5:
                 balance = customer.get_account().get_balance()
                 loanAmount = customer.get_account().getLoanAmount()
@@ -207,35 +178,33 @@ class BankSystem(object):
                     print("You have insufficient funds")
             elif choice == 6:
                 loop = 0
-        print ("Exit account operations")
-
+        print("Exit account operations")
 
     def admin_menu(self, admin_name):
-        #print the options you have
-         print (" ")
-         print ("Welcome Admin %s : Available options are:" %admin_name)
-         print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-         print ("1) Transfer money")
-         print ("2) Customer account operations")
-         print ("3) Customer profile settings")
-         print ("4) Admin profile settings")
-         print ("5) Delete customer")
-         print ("6) Print all customers detail")
-         print ("7) Sign out")
-         print (" ")
-         option = int(input ("Choose your option: "))
-         return option
-
+        # print the options you have
+        print(" ")
+        print("Welcome Admin %s : Available options are:" % admin_name)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("1) Transfer money")
+        print("2) Customer account operations")
+        print("3) Customer profile settings")
+        print("4) Admin profile settings")
+        print("5) Delete customer")
+        print("6) Print all customers detail")
+        print("7) Sign out")
+        print(" ")
+        option = int(input("Choose your option: "))
+        return option
 
     def run_admin_options(self, admin):
-                                
+
         loop = 1
         while loop == 1:
             choice = self.admin_menu(admin.get_name())
             if choice == 1:
                 pass
             elif choice == 2:
-                #STEP A.5
+                # STEP A.5
                 customer_name = input("\nPlease input customer name :\n")
                 customer = self.search_customers_by_name(customer_name)
                 if customer != None:
@@ -243,13 +212,13 @@ class BankSystem(object):
                 if account != None:
                     account.run_account_options()
             elif choice == 3:
-                #STEP A.6
+                # STEP A.6
                 customer_name = input("\nPlease input customer name :\n")
                 customer = self.search_customers_by_name(customer_name)
                 if customer != None:
                     customer.run_profile_options()
             elif choice == 4:
-                #STEP A.7
+                # STEP A.7
                 admin.run_profile_options()
             elif choice == 5:
                 if admin.has_full_admin_right() == True:
@@ -258,25 +227,116 @@ class BankSystem(object):
                     if customer_account != None:
                         self.customers_list.remove(customer_account)
                     else:
-                        print("\nOnly administrators with full admin rights can remove a customer from the bank system!\n")
+                        print(
+                            "\nOnly administrators with full admin rights can remove a customer from the bank system!\n")
             elif choice == 6:
-                #STEP A.9
+                # STEP A.9
                 self.print_all_accounts_details()
             elif choice == 7:
                 loop = 0
-        print ("Exit account operations")
+        print("Exit account operations")
 
-                    
-                        
-                    
     def print_all_accounts_details(self):
-            # list related operation - move to main.py
-            i = 0
-            for c in self.customers_list:
-                i+=1
-                print('/n %d. ' %i, end=' ')
-                c.print_details()
-                print("------------------------")
+        # list related operation - move to main.py
+        i = 0
+        for c in self.customers_list:
+            i += 1
+            print('/n %d. ' % i, end=' ')
+            c.print_details()
+            print("------------------------")
+
+
+class MainMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = Label(self, text="Enter Details")
+        var = tk.IntVar()
+        logintxt = Label(self, text="Login:")
+        passtxt = Label(self, text="Password:")
+        inputlogin = Entry(self)
+        inputpass = Entry(self)
+        adminradio = Radiobutton(self, text="Admin", variable=var, value=1)
+        userradio = Radiobutton(self, text="Customer", variable=var, value=2)
+        adminradio.deselect()
+        userradio.deselect()
+
+        def submitfunc():
+            if var.get() == 1:
+                admin_login(inputlogin.get(), inputpass.get())
+            elif var.get() == 2:
+                customer_login(inputlogin.get(), inputpass.get())
+            else:
+                label.config(text="Please Select Account Type")
+
+        submit = Button(self, text="Submit", command=lambda : submitfunc())
+
+        label.grid(row=0, column=0)
+        logintxt.grid(row=1, column=0)
+        inputlogin.grid(row=1, column=1)
+        passtxt.grid(row=2, column=0)
+        inputpass.grid(row=2, column=1)
+        adminradio.grid(row=3, column=0)
+        userradio.grid(row=4, column=0)
+        submit.grid(row=3, column=1)
+
+        def customer_login(name, password):
+            # STEP A.1
+            found_customer = search_customers_by_name(name)
+            if found_customer == None:
+                label.config(text="User Not Found")
+            else:
+                if found_customer.check_password(password) == True:
+                    controller.show_frame(CustomerMenu)
+                else:
+                    label.config(text="Incorrect Password")
+
+        def search_customers_by_name(customer_name):
+            # STEP A.2
+            found_customer = None
+            for a in customers_list:
+                name = a.get_name()
+                if name == customer_name:
+                    found_customer = a
+                    break
+            if found_customer == None:
+                print("\nThe customer %s does not exist! Try again...\n" % customer_name)
+            return found_customer
+
+        def admin_login(name, password):
+            # STEP A.3
+            found_admin = search_admin_by_name(name)
+            if found_admin == None:
+                label.config(text="User Not Found")
+            else:
+                if found_admin.check_password(password) == True:
+                    controller.show_frame(AdminMenu)
+                else:
+                    label.config(text="User Not Found")
+
+        def search_admin_by_name(admin_name):
+            # STEP A.4
+            found_admin = None
+            for a in admins_list:
+                name = a.get_name()
+                if name == admin_name:
+                    found_admin = a
+                    break
+            if found_admin == None:
+                print("\nThe admin %s does not exist! Try again...\n" % admin_name)
+            return found_admin
+
+
+class CustomerMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = Label(self, text="Login")
+        label.pack(pady=10, padx=10)
+
+class AdminMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = Label(self, text="Login")
+        label.pack(pady=10, padx=10)
 
 app = BankSystem()
 app.run_main_option()
